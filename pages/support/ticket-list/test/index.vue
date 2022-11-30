f90bd6aea3a529f2cb9fd40877be51d33e6397ad
Sos <sosmanucharyan89@gmail.com>
2022-11-30 13:18:04 +0400

    test

--- pages/support/ticket-list/test/index.vue
+++ pages/support/ticket-list/test/index.vue
@@ -3,7 +3,7 @@
   <div class="row">
     <div class="col-6">
       <template v-for="user in users">
-        <p>{{user.user_id}} {{ user.product_id}}</p>
+        <p><b>User id:</b> {{user.user_id}} <b>Product id:</b> {{ user.product_id}}</p>
       </template>
     </div>
     <div class="col-6">
@@ -20,60 +20,56 @@ export default {
   data(){
     return {
       users: [
-        {user_id: '20ad30', product_id: '103a'},
-        {user_id: '30ad20', product_id: '103ahg'},
-        {user_id: '20ad30', product_id: null},
-        {user_id: 'ad3020', product_id: '103ah11'},
-        {user_id: '20ad30', product_id: '103a12'},
-        {user_id: '20ad30', product_id: null},
-        {user_id: '30ad20', product_id: '103ah32'},
-        {user_id: '20ad30', product_id: '103a34'},
-        {user_id: '20ad30', product_id: null},
-        {user_id: 'ad3020', product_id: '103a45'},
-        {user_id: '20ad30', product_id: '103a233'},
-        {user_id: 'ad3020', product_id: null},
-        {user_id: '30ad20', product_id: '103ag4334'},
-        {user_id: 'ad3020', product_id: '103a2321'},
-        {user_id: '20ad30', product_id: null},
-        {user_id: '30ad20', product_id: '103ah345'},
-        {user_id: '20ad30', product_id: '103aj4344'},
-        {user_id: 'ad3020', product_id: '103ag77'},
+        { user_id: '20ad30', product_id: '103a' },
+        { user_id: '30ad20', product_id: '103ahg' },
+        { user_id: '20ad30', product_id: null },
+        { user_id: 'ad3020', product_id: '103ah11' },
+        { user_id: '20ad30', product_id: '103a12' },
+        { user_id: '20ad30', product_id: null },
+        { user_id: '30ad20', product_id: '103ah32' },
+        { user_id: '20ad30', product_id: '103a34' },
+        { user_id: '20ad30', product_id: null },
+        { user_id: 'ad3020', product_id: '103a45' },
+        { user_id: '20ad30', product_id: '103a233' },
+        { user_id: 'ad3020', product_id: null },
+        { user_id: '30ad20', product_id: '103ag4334' },
+        { user_id: 'ad3020', product_id: '103a2321' },
+        { user_id: '20ad30', product_id: null },
+        { user_id: '30ad20', product_id: '103ah345' },
+        { user_id: '20ad30', product_id: '103aj4344' },
+        { user_id: 'ad3020', product_id: '103ag77' },
       ],
-      res: [],
+      user: []
     }
   },
   methods: {
     sort() {
-      // for (const a of this.users) {
-      //   let arr = []
-      //   for (const b of this.users) {
-      //     if (a.user_id === b.user_id) {
-      //       arr.push(b)
-      //     }
-      //   }
-      //   console.log(arr)
-      //
-      //   // this.res.push(arr)
-      //
-      // }
-      this.users.map((item) => {
-        let arr = []
-        this.users.map ((i) => {
-          if (item.user_id === i.user_id){
-
-            arr.push(i)
-
+      let res = []
+      this.users.map((el) => {
+        const arr = {user_id: el.user_id, products: []}
+        this.users.map ((el2) => {
+          if (el.user_id === el2.user_id){
+            if (el2.product_id){
+              arr.products.push(el2.product_id)
+            }
           }
         })
-
-        this.res.push(arr)
-
+        res.push(arr)
       })
-      // console.log(this.res[0])
-
+      const uniqueIds = [];
+      const unique = res.filter(element => {
+        const isDuplicate = uniqueIds.includes(element.user_id);
+        if (!isDuplicate) {
+          uniqueIds.push(element.user_id);
+          return true;
+        }
+        return false;
+      });
+      unique.map((el)=> {
+        this.user.push(el)
+      })
     },
   }
-
 }
 </script>
 
